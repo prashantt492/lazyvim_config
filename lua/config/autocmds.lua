d3d837b5ae56fcccb6cf21e1739ee2176ad6de35
Prashant <prashantt492@gmail.com>
2023-06-12 23:24:32 +0530

    update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,7 +1,8 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-require("lspconfig").texlab.setup({})
+
+-- Disabling treesitter highlighting for vimtex
 -- require 'nvim-treesitter.configs'.setup {
 --        highlight = {
 --          enable = true,
@@ -9,3 +10,6 @@ require("lspconfig").texlab.setup({})
 --        },
 -- more stuff here
 -- }
+
+-- LSP setups
+require("lspconfig").texlab.setup({})
---
+++ lua/plugins/noice.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    cmdline = {
+      enabled = false,
+    },
+    popupmenu = {
+      enabled = false,
+    },
+    messages = {
+      enabled = false,
+    },
+  },
+}
